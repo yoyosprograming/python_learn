2a7ad5cc183852acec86e71d0c136a3e2a3d0837
yoyos <yoyos_nchu@126.com>
2020-08-04 23:31:19 +0800

    first test

---
+++ File/operate.csv.py
@@ -0,0 +1,20 @@
+import csv
+import codecs
+
+
+data = [
+    ("测试1",'软件测试工程师'),
+    ("测试2",'软件测试工程师'),
+    ("测试3",'软件测试工程师'),
+    ("测试4",'软件测试工程师'),
+    ("测试5",'软件测试工程师'),
+]
+
+f2 = codecs.open('2222.csv','a+','gbk')
+writer = csv.writer(f2)
+for i in data:
+    writer.writerow(i)
+    print(i)
+
+
+f2.close()
